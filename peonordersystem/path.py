7b194bb4e7b7ae4f54b995cf823bbf48a14fd207
Carl McGraw <GMMasterCarl@gmail.com>
2013-09-09 17:39:09 -0700

    Resolved Issue: #1 resolved by os.getcwd()

--- peonordersystem/path.py
+++ peonordersystem/path.py
@@ -1,12 +1,13 @@
-
+"""This module provides support for accessing external files
+for generating the UI and its components.
+"""
 import os
 
-SYSTEM_DIRECTORY_PATH = os.path.expanduser(
-   "~/peon-order-system/test suite/custom") 
+SYSTEM_DIRECTORY_PATH = os.getcwd() 
 
-SYSTEM_XML_PATH = SYSTEM_DIRECTORY_PATH + '/data/ui/'
+SYSTEM_UI_PATH = SYSTEM_DIRECTORY_PATH + '/data/ui/'
 SYSTEM_FILE_PATH = SYSTEM_DIRECTORY_PATH + '/data/files/'
-MAIN_UI_PATH = SYSTEM_XML_PATH + 'PeonOrderSystemWindow.ui'
+MAIN_UI_PATH = SYSTEM_UI_PATH + 'PeonOrderSystemWindow.ui'
 
 MENU_DATA = SYSTEM_FILE_PATH + 'menu_document.json'
-OPTIONS_DATA = SYSTEM_XML_PATH + 'option_choices.json'
+OPTIONS_DATA = SYSTEM_UI_PATH + 'option_choices.json'
