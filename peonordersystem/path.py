210984a81c0cc5b3af1f0ae90f6f8f64cb6e9fff
Carl McGraw <GMMasterCarl@gmail.com>
2013-09-11 13:19:46 -0700

    Update path

--- peonordersystem/path.py
+++ peonordersystem/path.py
@@ -5,9 +5,12 @@ import os
 
 SYSTEM_DIRECTORY_PATH = os.getcwd() 
 
-SYSTEM_UI_PATH = SYSTEM_DIRECTORY_PATH + '/data/ui/'
-SYSTEM_FILE_PATH = SYSTEM_DIRECTORY_PATH + '/data/files/'
-MAIN_UI_PATH = SYSTEM_UI_PATH + 'PeonOrderSystemWindow.ui'
+SYSTEM_DATA_PATH = SYSTEM_DIRECTORY_PATH + '/data/'
 
+SYSTEM_UI_PATH = SYSTEM_DATA_PATH + 'ui/'
+SYSTEM_FILE_PATH = SYSTEM_DATA_PATH + 'files/'
+SYSTEM_ORDERS_PATH = SYSTEM_DATA_PATH + 'orders/'
+
+MAIN_UI_PATH = SYSTEM_UI_PATH + 'PeonOrderSystemWindow.ui'
 MENU_DATA = SYSTEM_FILE_PATH + 'menu_document.json'
 OPTIONS_DATA = SYSTEM_UI_PATH + 'option_choices.json'
