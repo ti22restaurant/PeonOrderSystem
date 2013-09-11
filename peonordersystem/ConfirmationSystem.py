2178b6679df2ecd6b0dfc712573880899e31fc02
Carl McGraw <GMMasterCarl@gmail.com>
2013-09-11 13:27:29 -0700

    Added comments to ConfirmationSystem module

--- peonordersystem/ConfirmationSystem.py
+++ peonordersystem/ConfirmationSystem.py
@@ -1,4 +1,13 @@
-'''
+'''This module provides functionality for the confirmation system.
+Importing the module implicitly builds the necessary directories
+to store in the data folder of the project. This import builds 
+the following files ontop of data if they do not already exist
+
+'...data/orders/{year}/{month}/{day}/'
+
+All of this modules functions operate on this as the parent
+directory. Each time this module is invoked and the year, month
+or day changes it instantiates a new directory.
 
 @author: Carl McGraw
 @contact: cjmcgraw@u.washington.edu
@@ -24,7 +33,17 @@ for value in current_date:
 
 
 def generate_files():
-    """
+    """Generates the necessary confirmed and
+    checkout files. If those files already exist
+    in the parent directory than this function
+    returns information regarding those for loading
+    purposes.
+    
+    @return: 2-tuple. Each entry is a dict, where the
+    first entry represents the table orders and the
+    second entry represents the togo orders. Each
+    dict has a key that maps to a list of the MenuItem
+    objects that were present in that order.
     """
     table_orders = {}
     togo_orders = {}
@@ -54,6 +73,10 @@ def generate_files():
     return (table_orders, togo_orders)
 
 def order_confirmed(order_name, order_list):
+    """Confirms an order by dumping a binary
+    file representing the confirmed order into
+    the parent directories confirmed folder.
+    """
     curr_directory = directory + '/confirmed/'
     
     curr_file = open(curr_directory + order_name, 'w')
