3cd6ae63fcba9ee3eaeb9f24147a1f2d2df1ca70
Carl McGraw <GMMasterCarl@gmail.com>
2013-11-16 16:03:44 -0800

    Issue #46. Branch Iss46. Step two implementation created necessary constants in the Settings file.

--- peonordersystem/Settings.py
+++ peonordersystem/Settings.py
@@ -6,6 +6,17 @@ throughout the PeonOrderSystem project.
 @contact: cjmcgraw@u.washington.edu
 @version: 1.0
 """
+# in milliseconds, 10 minutes default
+RESERVATION_UPDATE_TIME_FRAME = 1000 * 60 * 10
+# in seconds, 12000 sec(20 minutes) default
+RESERVATION_NOTIFICATION_TIME_MAX = RESERVATION_UPDATE_TIME_FRAME / 1000 * \
+                                      2.0 - 1.0
+# in seconds, 0 seconds
+RESERVATION_NOTIFICATION_TIME_MIN = 0.0
+
+# notification time frame as str
+RESERVATION_NOTIFICATION_TIME_FRAME_STR = \
+    str(round(RESERVATION_NOTIFICATION_TIME_MAX / 60)) + ' minutes'
 
 STANDARD_TEXT = 500
 STANDARD_TEXT_BOLD = STANDARD_TEXT + 300
