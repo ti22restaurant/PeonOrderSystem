83a916b910d6fc6b6d56d81b62b90acdcbeb1c58
Carl McGraw <GMMasterCarl@gmail.com>
2013-12-17 21:09:29 -0800

    Issue #52. Branch Iss52. Step one implementation. Updated necessary file systems. Added new testing classes.

---
+++ test/peonordersystem/ConfirmationSystem_Test.py
@@ -0,0 +1,34 @@
+"""
+
+@author: Carl McGraw
+@contact: cjmcgraw( at )u.washington.edu
+"""
+
+import os
+import sqlite3
+import unittest
+
+
+from src.peonordersystem import path
+from src.peonordersystem import ConfirmationSystem
+
+TABS = '    '
+
+
+class ConfirmationSystemTest(unittest.TestCase):
+    """
+
+    """
+    def setUp(self):
+        """
+
+        @return:
+        """
+        print 'sup'
+
+    def tearDown(self):
+        """
+
+        @return:
+        """
+        pass
