f8e205610e523eedda105f70a5d87d8c721b6c17
Carl McGraw <GMMasterCarl@gmail.com>
2014-02-17 16:29:58 -0800

    Issue #55. Branch Iss55. Update: Updated abstract base class for Builder to include all necessary functionality for being operated on by the UI object.

--- src/peonordersystem/interface/builder/abc/Builder.py
+++ src/peonordersystem/interface/builder/abc/Builder.py
@@ -45,21 +45,6 @@ class AbstractBuilder(object):
         pass
 
     @abstractmethod
-    def add_from_file(self, path, title):
-        """Adds the builder data from the given
-        path with the given title.
-
-        @param path: str representing the path
-        to the builder xml file.
-
-        @param title: title representing the
-        title associated with the builder.
-
-        @return: None
-        """
-        pass
-
-    @abstractmethod
     def connect_signals(self, connect_ref):
         """Connects the signals to the necessary
         method calls.
@@ -107,6 +92,93 @@ class AbstractBuilder(object):
 
         @keyword styles: list representing the
         styles to apply to the status.
+
+        @return: None
+        """
+        pass
+
+    @abstractmethod
+    def get_menu_data(self):
+        """Gets the stored menu data.
+
+        @return: dict representing
+        the menu data.
+        """
+        pass
+
+    @abstractmethod
+    def get_categories_data(self):
+        """Gets the stored categories
+        data.
+
+        @return: dict representing the
+        categories data.
+        """
+        pass
+
+    @abstractmethod
+    def get_options_data(self):
+        """Gets the stored options
+        data.
+
+        @return: dict representing the
+        options data
+        """
+        pass
+
+    @abstractmethod
+    def get_discount_templates_data(self):
+        """Gets the stored discount templates
+        data.
+
+        @return: dict representing the stored
+        discount templates data.
+        """
+        pass
+
+    @abstractmethod
+    def update_menu_data(self, updated_menu_data):
+        """Updates the stored menu data.
+
+        @param updated_menu_data: dict
+        representing the updated data.
+
+        @return: None
+        """
+        pass
+
+    @abstractmethod
+    def update_categories_data(self, updated_categories_data):
+        """Updates the stored categories data.
+
+        @param updated_categories_data: dict
+        representing the updated categories
+        data.
+
+        @return: None
+        """
+        pass
+
+    @abstractmethod
+    def update_options_data(self, updated_options_data):
+        """Updates the stored options data.
+
+        @param updated_options_data: dict
+        representing the updated options
+        data.
+
+        @return: None
+        """
+        pass
+
+    @abstractmethod
+    def update_discount_templates_data(self, updated_discount_templates):
+        """Updates the stored discount
+        templates data.
+
+        @param updated_discount_templates: dict
+        representing the updated discount templates
+        data.
 
         @return: None
         """
