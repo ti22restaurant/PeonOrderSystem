f19502b7c5ad4cb4aa580cc9de6d90da6ae4b9b1
Carl McGraw <GMMasterCarl@gmail.com>
2014-03-30 20:37:10 -0700

    Added launcher

---
+++ src/peonordersystem/confirmationSystem/printers/formatters/KitchenFormatter.py
@@ -0,0 +1,24 @@
+"""
+Copyright Carl McGraw 2014
+
+
+
+
+@author: Carl McGraw
+@contact: cjmcgraw( at )u.washington.edu
+@version: 1.0
+"""
+from .abc.formatter import AbstractFormatter
+
+
+class Formatter(AbstractFormatter):
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        @return:
+        """
+        pass
