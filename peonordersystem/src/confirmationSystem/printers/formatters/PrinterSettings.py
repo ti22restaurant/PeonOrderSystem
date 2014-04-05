70d7bb613951e67e017f7bd228c6f46d4d592852
Carl McGraw <GMMasterCarl@gmail.com>
2014-04-05 02:55:03 -0700

    Issue #15. Branch Iss15. Step twelve: Phase eight implementation - created printerSettings module that provides constants representing the physical characteristics of the printer.

---
+++ peonordersystem/src/confirmationSystem/printers/formatters/PrinterSettings.py
@@ -0,0 +1,12 @@
+"""This module defines constants that
+are described as constraints by the
+physical printer.
+
+@author: Carl McGraw
+@contact: cjmcgraw( at )u.washington.edu
+@version: 1.0
+"""
+from reportlab.lib.units import cm
+
+# Constants used for defining the paper characteristics
+DEFAULT_FRONT_PRINTER_WIDTH = 7.2 * cm
