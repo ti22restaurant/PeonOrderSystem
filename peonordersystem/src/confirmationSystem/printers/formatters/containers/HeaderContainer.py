94991db8fc2b69b65fae7947016035c19c986e1a
Carl McGraw <GMMasterCarl@gmail.com>
2014-04-05 20:08:25 -0700

    Issue #15. Branch Iss15. Step thirteen: Phase three implementation - Updated HeaderContainer by removing redundancy already covered by ReceiptContainer abc. Further reduced reliance on certain constants, and avoided "magic numbers" for dict keys in the code.

--- peonordersystem/src/confirmationSystem/printers/formatters/containers/HeaderContainer.py
+++ peonordersystem/src/confirmationSystem/printers/formatters/containers/HeaderContainer.py
@@ -6,11 +6,8 @@ for a receipt.
 @contact: cjmcgraw( at )u.washington.edu
 @version: 1.0
 """
-import jsonpickle
 from os.path import join
 from datetime import datetime
-from reportlab.platypus.frames import Frame
-from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import (Paragraph, Image)
 
 from peonordersystem.src.confirmationSystem.printers.formatters.PrinterSettings \
@@ -26,17 +23,18 @@ from peonordersystem.src.Settings import (RECEIPT_IMAGE_FILE_NAME,
 from peonordersystem.SystemPath import (SYSTEM_MEDIA_PATH,
                                         SYSTEM_TEMPLATE_RECEIPT_HEADER_PATH)
 
-from .abc.Container import Container
+from .abc.ReceiptContainer import ReceiptContainer
 
 
-class HeaderContainer(Container):
+class HeaderContainer(ReceiptContainer):
     """Defines the HeaderContainer
     that is used to generate, store
     and write receipt Headers.
     """
-    DEFAULT_HEADER_HEIGHT = 250
+    CFG_DATE_KEY = 'DATE'
+    CFG_TIME_KEY = 'TIME'
 
-    DEFAULT_STYLE = getSampleStyleSheet()['Normal']
+    DEFAULT_HEADER_HEIGHT = 250
     HEADER_IMAGE_PATH = join(SYSTEM_MEDIA_PATH, RECEIPT_IMAGE_FILE_NAME)
 
     def __init__(self, x, y, width=DEFAULT_FRONT_PRINTER_WIDTH):
@@ -55,50 +53,10 @@ class HeaderContainer(Container):
         this value is equal to the width of
         the printer page.
         """
-        self._area = (width, self.DEFAULT_HEADER_HEIGHT)
-        self._start_point = (x, y)
-        self._frame = Frame(x, y, width, self.DEFAULT_HEADER_HEIGHT)
-        self._header = self._create_header()
+        super(HeaderContainer, self).__init__(x, y, width,
+                                              self.DEFAULT_HEADER_HEIGHT)
 
-    @property
-    def start_point(self):
-        """Gets the point that
-        represents the initial point
-        for this frame.
-
-        @return: 2 tuple of (int, int)
-        representing the x and y coordinates
-        that is the starting point of the
-        area written.
-        """
-        return self._start_point
-
-    @property
-    def area(self):
-        """Gets the values that
-        represent the area of the
-        frame.
-
-        @return: 2 tuple of (int, int)
-        representing the width and height
-        of the frame respectively.
-        """
-        return self._area
-
-    def write(self, canvas):
-        """Writes the frame to the
-        given canvas.
-
-        @param canvas: reportlab.pdfgen.canvas.Canvas
-        object that represents the canvas that the
-        container should write the frame to.
-
-        @return: None
-        """
-        self._check_canvas(canvas)
-        self._frame.addFromList(self._header, canvas)
-
-    def _create_header(self):
+    def create_header(self):
         """Creates the header that represents
         the data this container will write in
         its frame.
@@ -143,7 +101,7 @@ class HeaderContainer(Container):
                         RECEIPT_HEADER_TITLE_TEMPLATE_FILE_NAME)
         cfg_path = join(SYSTEM_TEMPLATE_RECEIPT_HEADER_PATH,
                         RECEIPT_HEADER_TITLE_CFG_FILE_NAME)
-        paragraph_text = self._format_rml_file(rml_path, cfg_path)
+        paragraph_text = self.format_rml_file(rml_path, cfg_path)
         return Paragraph(paragraph_text, self.DEFAULT_STYLE)
 
     def _create_header_timestamp(self):
@@ -178,52 +136,10 @@ class HeaderContainer(Container):
         config data for the timestamp.
         """
         curr_datetime = datetime.now()
-        cfg_data['DATE'] = curr_datetime.strftime(cfg_data['DATE'])
-        cfg_data['TIME'] = curr_datetime.strftime(cfg_data['TIME'])
+
+        date_str = curr_datetime.strftime(cfg_data[self.CFG_DATE_KEY])
+        cfg_data[self.CFG_DATE_KEY] = date_str
+
+        time_str = curr_datetime.strftime(cfg_data[self.CFG_TIME_KEY])
+        cfg_data[self.CFG_TIME_KEY] = time_str
         return cfg_data
-
-    def _format_rml_file(self, rml_file_path, cfg_file_path):
-        """Formats the rml file at the given path
-        with the data from the given config file
-        path.
-
-        @param rml_file_path: str representing the
-        path to the rml file to be formatted.
-
-        @param cfg_file_path: str representing the
-        path to the cfg file to be accessed for the
-        format data.
-
-        @return: str representing the formatted rml
-        file.
-        """
-        data = self._get_rml_data(rml_file_path)
-        frmt = self._get_cfg_data(cfg_file_path)
-        return data.format(**frmt)
-
-    def _get_rml_data(self, rml_file_path):
-        """Gets the rml data from the given
-        rml file path.
-
-        @param rml_file_path: str representing
-        the file to be retrieved.
-
-        @return: str representing the data contained
-        within the given rml file path location.
-        """
-        with open(rml_file_path, 'r') as data_file:
-            return data_file.read()
-
-    def _get_cfg_data(self, cfg_file_path):
-        """Gets the cfg data from the given
-        cfg file path.
-
-        @param cfg_file_path: str representing
-        the file to be retrieved and parsed.
-
-        @return: dict representing the formatting
-        data stored in the config file.
-        """
-        with open(cfg_file_path, 'r') as data_file:
-            data = data_file.read()
-            return jsonpickle.decode(data)
