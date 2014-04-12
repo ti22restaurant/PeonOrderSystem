db0a35f651f090df4c06f737953456d4a48b9911
Carl McGraw <GMMasterCarl@gmail.com>
2014-04-12 05:06:14 -0700

    Issue #15. Branch Iss15. Updated Formatter module file name.

--- peonordersystem/src/confirmationSystem/printers/formatters/KitchenFormatter.py
+++ peonordersystem/src/confirmationSystem/printers/formatters/KitchenFormatter.py
@@ -8,7 +8,7 @@ Copyright Carl McGraw 2014
 @contact: cjmcgraw( at )u.washington.edu
 @version: 1.0
 """
-from .confirmationSystem.printers.formatters.abc.formatter import AbstractFormatter
+from .abc.Formatter import AbstractFormatter
 
 
 class Formatter(AbstractFormatter):
