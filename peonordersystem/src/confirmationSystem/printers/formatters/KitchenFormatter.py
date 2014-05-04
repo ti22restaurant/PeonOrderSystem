a284a0ca686e07b2f4a279505688cbddc0708a6d
Carl McGraw <GMMasterCarl@gmail.com>
2014-05-04 05:49:20 -0700

    Issue #15. Branch Iss15. Step nineteen - Phase one: Created KitchenFormatter class that combines the display containers into a formatted file to be sent to the kitchen.

--- peonordersystem/src/confirmationSystem/printers/formatters/KitchenFormatter.py
+++ peonordersystem/src/confirmationSystem/printers/formatters/KitchenFormatter.py
@@ -1,24 +1,66 @@
-"""
-Copyright Carl McGraw 2014
-
-
-
+"""This module defines the
+KitchenFormatter that is used
+to format files intended for
+the kitchen.
 
 @author: Carl McGraw
 @contact: cjmcgraw( at )u.washington.edu
 @version: 1.0
 """
-from .abc.Formatter import AbstractFormatter
+from os.path import join
+
+from peonordersystem.SystemPath import SYSTEM_TEMP_PATH
+from peonordersystem.src.Settings import KITCHEN_TICKET_FILE_NAME
+
+from .abc.AreaFormatter import AreaFormatter
+
+from .containers.PriorityContainer import PriorityContainer
+from .containers.TicketContainer import TicketContainer
 
 
-class Formatter(AbstractFormatter):
+class KitchenFormatter(AreaFormatter):
+    """This class takes display components
+    and formats them into a file for viewing
+    for the kitchen
     """
+    DEFAULT_FILE = join(SYSTEM_TEMP_PATH, KITCHEN_TICKET_FILE_NAME)
+    REQUIRED_KEYS = {'priority', 'order'}
 
-    """
+    @property
+    def file_path(self):
+        """Gets the file path
+        that the formatted file
+        is located at.
 
-    def __init__(self):
+        @return: str representing
+        the path to the generated
+        file.
         """
+        return self.DEFAULT_FILE
 
-        @return:
+    @property
+    def required_keys(self):
+        """Gets a set of str
+        representing the keys
+        required of the given
+        data.
+
+        @return: set of str
         """
-        pass
+        return self.REQUIRED_KEYS
+
+    def generate_display_areas(self, data):
+        """Generates the display containers
+        with the given data.
+
+        @param data: dict of str keys to
+        values that represent the data to
+        be displayed.
+
+        @return: None
+        """
+        priority = PriorityContainer(data)
+        ticket = TicketContainer(data)
+
+        self.add_display(ticket)
+        self.add_display(priority)
