1532601cc8008cc0826c275914f3e881b98cb938
Carl McGraw <GMMasterCarl@gmail.com>
2014-05-04 03:39:49 -0700

    Issue #15. Branch Iss15. Update: Provided start_point setter. Provided add_component and add_flowables methods that pushes their functionality into this base super class.

--- peonordersystem/src/confirmationSystem/printers/formatters/containers/abc/ReceiptContainer.py
+++ peonordersystem/src/confirmationSystem/printers/formatters/containers/abc/ReceiptContainer.py
@@ -6,7 +6,6 @@ class ReceiptContainer.
 @version: 1.0
 """
 from json import loads
-from abc import ABCMeta, abstractmethod
 
 from reportlab.platypus.frames import Frame
 from reportlab.lib.styles import ParagraphStyle
@@ -19,73 +18,117 @@ class ReceiptContainer(Container):
     for an object to be a useable
     ReceiptContainer.
     """
-
-    __metaclass__ = ABCMeta
-
     DEFAULT_STYLE = ParagraphStyle({'wordWrap': 1})
-    CENTER_STYLE = ParagraphStyle({'wordWrap': 1,
-                                   'alignment': 'TA_CENTER'})
+    CENTER_STYLE = ParagraphStyle({'wordWrap': 1, 'alignment': 'TA_CENTER'})
 
-    def __init__(self, x, y, width, height):
-        """Initializes the ReceiptContainer.
+    def __init__(self):
+        """Initializes the ReceiptContainer"""
+        self._width = 0.0
+        self._height = 0.0
 
-        @param x: int representing the initial
-        x value that the container frame should
-        be written at.
+        self._x = None
+        self._y = None
 
-        @param y: int representing the initial
-        y value that the container frame should
-        be written at
-
-        @param width: int representing the width
-        of the header container. This is also the
-        x coordinate representing its area.
-
-        @param height: int representing the height
-        of the header container. This is also the
-        y coordinate representing its area.
-        """
-        self._area = (width, height)
-        self._start_point = (x, y)
-        self._frame = Frame(x, y, width, height, topPadding=0, bottomPadding=0)
-        self._header = self.create_header()
-
-    @abstractmethod
-    def create_header(self):
-        """Creates the header that represents
-        the data this container will write in
-        its frame.
-
-        @return: list of reportlab.Flowable
-        objects that represents the data to
-        be written by the container.
-        """
-        pass
+        self._components = []
 
     @property
     def start_point(self):
         """Gets the point that
         represents the initial point
-        for this frame.
+        for this container
 
-        @return: 2 tuple of (int, int)
+        @return: 2 tuple of (float, float)
         representing the x and y coordinates
         that is the starting point of the
         area written.
         """
-        return self._start_point
+        return self._x, self._y
+
+    @start_point.setter
+    def start_point(self, coord):
+        """Sets the point that
+        represents the initial
+        point for this container.
+
+        @param coord: 2 tuple of
+        (float, float) representing
+        the x and y coordinates of
+        where the container should
+        be written on a cartesian
+        plane.
+
+        @return: None
+        """
+        self._x, self._y = coord
 
     @property
     def area(self):
         """Gets the values that
         represent the area of the
-        frame.
+        container
 
         @return: 2 tuple of (int, int)
         representing the width and height
         of the frame respectively.
         """
-        return self._area
+        return self._width, self._height
+
+    def add_component(self, component):
+        """Adds the given component
+        to the container.
+
+        @param component: Component
+        object to be added.
+
+        @return: None
+        """
+        self.add_flowables(component.flowables,
+                           component.width,
+                           component.height)
+
+    def add_flowables(self, flowables, width, height):
+        """Adds the given list of flowables,
+        with the specified width and height
+        to the container.
+
+        @param flowables: list of
+        reportlab.platypus.flowable
+        objects representing the displays
+        to be added to the container.
+
+        @param width: float representing
+        the width associated with the
+        widest flowable in the list.
+
+        @param height: float representing
+        the cumulative height of all flowables
+        if they were to be placed consecutively
+        without breaks.
+
+        @return: None
+        """
+        self._components += flowables
+        self._update_area(width, height)
+
+    def _update_area(self, width, height):
+        """Updates the area associated with
+        the container.
+
+        @param width: float representing
+        the width that the area must be
+        able to contain. The largest width
+        given will be used.
+
+        @param height: float representing
+        the height that the area must be
+        able to contain. The height will
+        be the cumulative height of all
+        displays.
+
+        @return: None
+        """
+        self._width = max(self._width, width)
+        self._height += height
 
     def write(self, canvas):
         """Writes the frame to the
@@ -98,7 +141,26 @@ class ReceiptContainer(Container):
         @return: None
         """
         self._check_canvas(canvas)
-        self._frame.addFromList(self._header, canvas)
+        self._check_start_point()
+
+        frame = Frame(self._x, self._y, self._width, self._height,
+                      topPadding=0, bottomPadding=0)
+        frame.addFromList(self._components, canvas)
+
+    def _check_start_point(self):
+        """Checks if a valid start
+        point has been specified.
+
+        @throws ValueError: if no
+        start point has been defined
+        for this container.
+
+        @return: bool value representing
+        if the test was passed.
+        """
+        if self._x is None and self._y is None:
+            raise ValueError("The start_point for the container has yet to be set!")
+        return True
 
     def format_rml_file(self, rml_file_path, cfg_file_path):
         """Formats the rml file at the given path
