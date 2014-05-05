9b437592e879b2ae0565b15f8f65d7aa7c8a5aeb
Carl McGraw <GMMasterCarl@gmail.com>
2014-05-04 20:45:47 -0700

    Issue #15. Branch Iss15. Step twenty: Included default constructor in LocationPrinter superclass.

--- peonordersystem/src/confirmationSystem/printers/components/abc/LocationPrinter.py
+++ peonordersystem/src/confirmationSystem/printers/components/abc/LocationPrinter.py
@@ -7,6 +7,9 @@ base class for the LocationPrinter
 """
 from abc import ABCMeta, abstractmethod
 
+from peonordersystem.src.confirmationSystem.printers.adapters.PrinterAdapter \
+    import PrinterAdapter
+
 
 class AbstractLocationPrinter(object):
     """Describes the basic functionality
@@ -16,13 +19,23 @@ class AbstractLocationPrinter(object):
 
     __metaclass__ = ABCMeta
 
+    def __init__(self, printer_name):
+        """Initializes the location printer
+        with the given name.
+
+        @param printer_name: str representing
+        the name to be associated with the
+        location printer.
+        """
+        self._printer = PrinterAdapter(printer_name)
+
     @abstractmethod
-    def send_to_printer(self, data):
+    def send_to_printer(self, file_path):
         """Sends the given data to
         the printer.
 
-        @param data: str data to be
-        sent to the printer.
+        @param file_path: str representing
+        the file path to be printed.
 
         @return: bool value representing
         if the print was successful.
