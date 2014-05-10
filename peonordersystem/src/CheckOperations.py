edc0f3ca10f323b3e2e035a7cca96f89caa8fa00
Carl McGraw <GMMasterCarl@gmail.com>
2014-05-09 18:11:12 -0700

    Issue #15. Branch Iss15. Step twenty two: Provided get_totals function in CheckOperations module.

--- peonordersystem/src/CheckOperations.py
+++ peonordersystem/src/CheckOperations.py
@@ -1,14 +1,36 @@
-'''This module provides the basic functions
+"""This module provides the basic functions
 that calculate the total and taxes on any
 given check.
 
 @author: Carl McGraw
 @contact: cjmcgraw@u.washington.edu
 @version: 1.0
-'''
+"""
 import math
 
 from Settings import SALES_TAX
+
+
+def get_totals(order_list):
+    """Gets a tuple of totals
+    that represent the totals
+    associated with the given
+    order list.
+
+    @param order_list: list of
+    MenuItem objects representing
+    the data to parse.
+
+    @return: three tuple of
+    (float, float, float)
+    representing the subtotal,
+    tax, and total respectively.
+    """
+    subtotal = get_order_subtotal(order_list)
+    tax = get_total_tax(subtotal)
+
+    return subtotal, tax, round(subtotal + tax, 2)
+
 
 def get_order_subtotal(order_list):
     """Gets the current subtotal of the given
@@ -25,7 +47,8 @@ def get_order_subtotal(order_list):
     for menu_item in order_list:
         total = total + menu_item.get_price()
         
-    return total
+    return round(total, 2)
+
 
 def get_total_tax(subtotal):
     """Gets the total tax to be added from
@@ -37,7 +60,9 @@ def get_total_tax(subtotal):
     @return: double representing the tax to be added
     to the total.
     """
-    return math.ceil(subtotal * SALES_TAX * 100) / 100
+    tax = math.ceil(subtotal * SALES_TAX * 100) / 100
+    return round(tax, 2)
+
 
 def get_total(order_list):
     """Gets the total of the order, tax included.
