c193f0f4b7afe93e1e9c8c4d1b48bc6e8c739505
Carl McGraw <cjmcgraw@uw.edu>
2014-08-18 03:05:41 -0700

    Iss #70: Updated DialogBuilder class to include a int signal representing the primary selection. Multiple functions to build required objects. Altered button methods to provide correct functionality.

--- peonordersystem/src/interface/dialogs/DialogBuilder.py
+++ peonordersystem/src/interface/dialogs/DialogBuilder.py
@@ -2,13 +2,15 @@
 for creating new widgets
 
 @author: Carl McGraw
-@email: cjmcgraw@u.washington.edu
+@contact: cjmcgraw@u.washington.edu
 @version: 1.1
 """
 
 from gi.repository import Gtk
 
 PACK_ARGS = (True, True, 5.0)
+
+PRIMARY_SELECTION_SIGNAL = 6000
 
 ACCEPT_RESPONSE = Gtk.ResponseType.ACCEPT
 CANCEL_RESPONSE = Gtk.ResponseType.CANCEL
@@ -31,11 +33,12 @@ def generate_default_button(name, f, size_request=(100, 50)):
     @return: Gtk.Button
     """
     result = Gtk.Button(name)
-    result.can_have_focus(True)
+    result.set_can_focus(True)
     result.set_size_request(*size_request)
 
     for action_type in DEFAULT_BUTTON_ACTION_TYPES:
         result.connect(action_type, f)
+    result.show_all()
     return result
 
 
@@ -52,6 +55,63 @@ def generate_default_dialog():
     """
     result = Gtk.Dialog()
     result.set_modal(True)
+    result.show_all()
+    return result
+
+
+def generate_treeview():
+    """Generates a treeview
+
+    @return: Gtk.TreeView
+    """
+    view = Gtk.TreeView()
+    view.show_all()
+    return view
+
+
+def generate_liststore(*types):
+    """Generates a liststore of
+    the given types
+
+    @param types: list of types
+    representing the types that
+    the list store should expect.
+
+    @return: Gtk.ListStore
+    """
+    return Gtk.ListStore(*types)
+
+
+def generate_text_columns(titles):
+    """Generates a list of text columns
+    that match expected data at each index
+    with the given title.
+
+    @param titles: list of str
+
+    @return: list of Gtk.TreeViewColumn
+    objects that have been generated.
+    """
+    cols = []
+
+    index = 0
+    for title in titles:
+        rend = Gtk.CellRendererText()
+        col = Gtk.TreeViewColumn(title, rend, text=index)
+        cols.append(col)
+        index += 1
+
+    return cols
+
+
+def generate_scrolled_window():
+    """Generates a scrolled window
+
+    @return: Gtk.ScrolledWindow
+    """
+    window = Gtk.ScrolledWindow()
+    window.show_all()
+    return window
 
 
 def generate_hbox():
@@ -60,7 +120,9 @@ def generate_hbox():
 
     @return: Gtk.HBox
     """
-    return Gtk.HBox()
+    box = Gtk.HBox()
+    box.show_all()
+    return box
 
 
 def generate_vbox():
@@ -69,4 +131,6 @@ def generate_vbox():
 
     @return: Gtk.VBox
     """
-    return Gtk.VBox()
+    box = Gtk.VBox()
+    box.show_all()
+    return box
